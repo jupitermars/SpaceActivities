99072f65236094a38e990edd5936ac8e3692e689
dawang3 <dawang3>
2016-03-15 16:22:59 -0700

    working with enemies

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var game = new Phaser.Game(1500, 1000, Phaser.AUTO);
-game.state.add('Load', boil.Load);
-game.state.add('Menu', boil.Menu);
+var game = new Phaser.Game(800, 600, Phaser.AUTO);
+//game.state.add('Load', boil.Load);
+//game.state.add('Menu', boil.Menu);
 game.state.add('Play', boil.Play);
 game.state.add('GameOver', boil.GameOver);
-game.state.start('Load'); 
+game.state.start('Play'); 
